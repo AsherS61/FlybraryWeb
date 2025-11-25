3d28228fbe8498c606ca778f3e2afbf20658f470
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-11-25 12:18:38 +0700

    fix::::

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function SignInPage() {
     const router = useRouter();
