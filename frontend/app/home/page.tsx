de472701c7f8967602b25bb24e54b21c74da7b1f
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-11-28 19:49:52 +0700

    help6

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -3,13 +3,17 @@
 // import next
 import { useRouter } from 'next/navigation'
 import BannerSection from './components/BannerSection'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
   // Primary variables
   const router = useRouter()
 
+  const { data: session } = useSession();
+  console.log("USER PROFILE: ", session)
+
   return (
-    <main className='flex flex-col leading-none'>
+    <main className='flex flex-col leading-none mt-20'>
         HOME PAGE
     </main>
   )
