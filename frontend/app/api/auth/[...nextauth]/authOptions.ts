0628a80b3823fe7d98c3dabe430ba46734b6e5f2
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-12-06 14:16:27 +0700

    fix:  auth options

--- frontend/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,8 +1,13 @@
+import { findOrCreateUser } from "@/libs/auth";
 import { NextAuthOptions, Session } from "next-auth";
 import LineProvider from "next-auth/providers/line";
 
 interface LineProfile {
-    userId: string; role: string; name: string; picture: string; lineId: string; 
+  sub: string;
+  name: string;
+  picture: string;
+  email?: string;
+  lineId: string;
 }
 
 export const authOptions: NextAuthOptions = {
@@ -10,33 +15,72 @@ export const authOptions: NextAuthOptions = {
     LineProvider({
       clientId: process.env.LINE_CHANNEL_ID!,
       clientSecret: process.env.LINE_CHANNEL_SECRET!,
-    })
-  ],
-
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
+    }
+  )],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production'
+      }
+    }
+  },
   callbacks: {
     async jwt({ token, account, profile }) {
+      console.log("Line Account:", account);
+      console.log("LINE profile:", profile);
+      
       if (account && profile) {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ profile })
-          }
-        );
-        const data = await res.json();
+        token.accessToken = account.access_token;
+        token.idToken = account.id_token;
+        const lineProfile = profile as LineProfile;
 
-        token.user = data.user as LineProfile;         
-        token.access_token = account.access_token; 
+        const userFromDB = await findOrCreateUser({
+          displayName: lineProfile.name,    
+          picture: lineProfile.picture,
+          lineId: lineProfile.sub,   
+        });
+
+        console.log("USER: " , userFromDB);
+        
+        token.userId = userFromDB.data?._id;
+        token.role = userFromDB.data?.role;
+        token.name = userFromDB.data?.name;
+        token.picture = userFromDB.data?.profile;
+        token.sub = userFromDB.data?.lineId;
       }
+
+      console.log("TOKEN:", token)
 
       return token;
     },
-
     async session({ session, token }) {
-      session.user = token.user as LineProfile;
-      session.access_token = token.access_token as string;
+      console.log("SESSION CALLBACK: ", { token });
+      session.user.userId = token.userId as string;
+      session.user.role = token.role as string;
+      session.user.name = token.name as string;
+      session.user.picture = token.picture as string;
+      session.user.lineId = token.sub as string;
+
       return session;
-    }
-  }
+    },
+    async redirect({ url, baseUrl }) {
+      if (url.includes('/callback') && url.includes('code=')) return url;
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
 };
