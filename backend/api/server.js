e9ae29bb14df9456df8972188ab6ba3f4b487811
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-12-06 14:38:40 +0700

    fuck me

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,34 +1,3 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const cors = require("cors");
+import app from "./app.js";
 
-dotenv.config({ path: "./.env" });
-
-connectDB();
-
-const auth = require("./routes/auth.js");
-const books = require("./routes/books.js");
-const transactions = require("./routes/transactions.js");
-
-const app = express();
-
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-app.use(
-  cors({
-    origin: "https://flybrary-web-auto-frontend.vercel.app",
-    credentials: true,
-  })
-);
-
-app.use(cookieParser());
-
-// Mount routes
-app.use("/api/v1/books", books);
-app.use("/api/v1/auth", auth);
-app.use("/api/v1/transactions", transactions);
-
-module.exports = app;
+app.listen(3000, () => console.log("Server running"));
